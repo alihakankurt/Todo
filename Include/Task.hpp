81e4b78315f26f5f54ca656d9cf4e76f96843a2d
alihakankurt <kurtalihakan@outlook.com>
2024-08-24 23:33:31 +0300

    Add another constructor for `Task` and define alias for time_point

--- Include/Task.hpp
+++ Include/Task.hpp
@@ -6,18 +6,25 @@ namespace todo
 {
     class Task
     {
+        using time_point = std::chrono::time_point<std::chrono::system_clock>;
+
       public:
         Task(std::string_view description) :
             _description(description),
-            _createdAt(std::chrono::system_clock::now()),
+            _createdAt(time_point{}),
             _completedAt() {}
+
+        Task(std::string&& description, time_point createdAt, time_point completedAt) :
+            _description(std::move(description)),
+            _createdAt(createdAt),
+            _completedAt(completedAt) {}
 
         ~Task() = default;
 
         auto Description() const -> std::string_view { return _description; }
-        auto CreatedAt() const -> std::chrono::time_point<std::chrono::system_clock> { return _createdAt; }
-        auto IsCompleted() const -> bool { return _completedAt != std::chrono::time_point<std::chrono::system_clock>{}; }
-        auto CompletedAt() const -> std::chrono::time_point<std::chrono::system_clock> { return _completedAt; }
+        auto CreatedAt() const -> time_point { return _createdAt; }
+        auto IsCompleted() const -> bool { return _completedAt != time_point{}; }
+        auto CompletedAt() const -> time_point { return _completedAt; }
 
         auto Complete() -> void
         {
@@ -26,7 +33,7 @@ namespace todo
 
       private:
         std::string _description;
-        std::chrono::time_point<std::chrono::system_clock> _createdAt;
-        std::chrono::time_point<std::chrono::system_clock> _completedAt;
+        time_point _createdAt;
+        time_point _completedAt;
     };
 }
